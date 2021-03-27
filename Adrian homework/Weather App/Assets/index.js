6df3da4b93206860127d09c71df6a80c5825e612
HallerJohn <37416046+HallerJohn@users.noreply.github.com>
2021-03-27 13:33:43 -0700

    Added documentation.

--- Adrian homework/Weather App/Assets/index.js
+++ Adrian homework/Weather App/Assets/index.js
@@ -1,32 +1,47 @@
+// Store the elements related to the city search.
 var citySearch = document.querySelector(".city-search");
 var searchBTN = document.querySelector(".search-button");
 
+// Store the elements related to the search history.
 var clearBTN = document.querySelector(".clear-history-button");
 var historyContainer = document.querySelector("history-container");
 var searchHistory = [];
 
+// Store the elements related to the current city/weather.
 var cityName = document.querySelector(".name");
 var currentTemp = document.getElementById("temperature");
 var currentHumid = document.getElementById("humidity");
 var currentWindSpeed = document.getElementById("wind-speed");
 var currentUVIndex = document.getElementById("UV-index");
 
+// Our key to access the openWeatherAPI.
 const API_KEY = "d49cc0dfd8837f8bf387ee4b861273fb";
 
+// When the search button is clicked.
 searchBTN.addEventListener("click", function(){
+    // Call the API.
+    // The purpose of this call is to get the name/latitude/longitude of the city.
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + citySearch.value + "&appid=" + API_KEY)
     .then(response => response.json())
     .then(data => {
+        // Store the name/latitude/longitude in variables.
         cityName.textContent = data['name'];
         var lon = data['coord']['lon'];
         var lat = data['coord']['lat'];
+        // Make a new API call.
+        // The purpose of this call is to get all the weather data.
+        // Note: Some of the weather data is available from the previous call but not all of it.
+        // So we need to make a different call using the coordinates.
         fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY + "&units=imperial")
         .then(response => response.json())
         .then(data => {
+            // Log all of the data from our API call.
             console.log(data);
+            // Get the current date.
             var date = new Date(data['current']['dt']*1000).toLocaleDateString();
             cityName.textContent += " " + date;
 
+            // Start retrieving all of the required weather data.
             currentTemp.textContent = data['current']['temp'] + "°F";
             currentHumid.textContent = data['current']['humidity'] + "%";
             currentWindSpeed.textContent = data['current']['wind_speed'] + "mph";
@@ -34,11 +49,3 @@ searchBTN.addEventListener("click", function(){
         })
     })
 })
-
-function getUVI(){
-    fetch("https://api.openweathermap.org/data/2.5/uvi?q=" + citySearch.value + "&appid=" + API_KEY)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-}
